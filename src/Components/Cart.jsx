37a5e7d5ad6376b163e1ee3f6b826bebadb29d07
Satya <satyaprakash01.gh@gmail.com>
2023-04-26 23:16:47 +0530

    cart work

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 import { getCart, updateCart, deleteCart } from '../Store/ActionCreators/CartActionCreators'
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 export default function Cart() {
     var [cart, setCart] = useState([]);
     var [total, setTotal] = useState(0);
@@ -9,6 +10,25 @@ export default function Cart() {
     var [final, setFinal] = useState(0);
     var dispatch = useDispatch()
     var carts = useSelector(state => state.CartStateData)
+    function updateRecord(id, op) {
+        var item = cart.find(item => item.id===id)
+        if(op==="dec" && item.qty===1)
+        return
+        else if(op=="dec"){
+            item.qty= item.qty-1
+            item.total= item.total-item.price
+        }
+        else{
+            item.qty = item.qty+1
+            item.total=item.total+item.price
+        }
+        dispatch(updateCart(item))
+        getApiData()
+    }
+    function deleteRecord(id) {
+        dispatch(deleteCart({id:id}))
+        getApiData()
+    }
     function getApiData() {
         dispatch(getCart())
         var data = carts.filter((item) => item.userid === localStorage.getItem("userid"));
@@ -27,7 +47,7 @@ export default function Cart() {
     }
     useEffect(() => {
         getApiData()
-    }, [carts])
+    }, [carts.length])
     return (
         <>
             <div className="container-fluid pt-5">
@@ -47,30 +67,31 @@ export default function Cart() {
                             </thead>
                             <tbody className="align-middle">
                                 {
-                                    cart.map((item, index)=>{
+                                    cart.map((item, index) => {
                                         return <tr key={index}>
-                                        <td className="align-middle"><img src={`/assets/productimages/${item.pic}`} alt="" style={{ width: "50px" }} />{item.name}</td>
-                                        <td className="align-middle">{item.color}</td>
-                                        <td className="align-middle">{item.size}</td>
-                                        <td className="align-middle">&#8377;{item.price}</td>
-                                        <td className="align-middle">
-                                            <div className="input-group quantity mx-auto" style={{ width: "100px" }}>
-                                                <div className="input-group-btn">
-                                                    <button className="btn btn-sm btn-primary btn-minus" >
-                                                        <i className="fa fa-minus"></i>
-                                                    </button>
+                                            <td className="align-middle"><img src={`/assets/productimages/${item.pic}`} alt="" style={{ width: "50px" }} />{item.name}</td>
+                                            <td className="align-middle">{item.color}</td>
+                                            <td className="align-middle">{item.size}</td>
+                                            <td className="align-middle">&#8377;{item.price}</td>
+                                            <td className="align-middle">
+                                                <div className="input-group quantity mx-auto" style={{ width: "100px" }}>
+                                                    <div className="input-group-btn">
+                                                        <button className="btn btn-sm btn-primary btn-minus" onClick={()=>updateRecord(item.id,"dec")}>
+                                                            <i className="fa fa-minus"></i>
+                                                        </button>
+                                                    </div>
+                                                    {/* <input type="text" className="form-control form-control-sm bg-secondary text-center" value={item.qty} /> */}
+                                                    <span style={{width:"30px"}}>{item.qty}</span>
+                                                    <div className="input-group-btn">
+                                                        <button className="btn btn-sm btn-primary btn-plus" onClick={()=>updateRecord(item.id,"inc")}>
+                                                            <i className="fa fa-plus"></i>
+                                                        </button>
+                                                    </div>
                                                 </div>
-                                                <input type="text" className="form-control form-control-sm bg-secondary text-center" value={item.qty} />
-                                                <div className="input-group-btn">
-                                                    <button className="btn btn-sm btn-primary btn-plus">
-                                                        <i className="fa fa-plus"></i>
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td className="align-middle">&#8377;{item.total}</td>
-                                        <td className="align-middle"><button className="btn btn-sm btn-primary"><i className="fa fa-times"></i></button></td>
-                                    </tr>
+                                            </td>
+                                            <td className="align-middle">&#8377;{item.total}</td>
+                                            <td className="align-middle"><button onClick={()=>deleteRecord(item.id)} className="btn btn-sm btn-primary"><i className="fa fa-times"></i></button></td>
+                                        </tr>
                                     })
                                 }
                             </tbody>
@@ -92,19 +113,19 @@ export default function Cart() {
                             <div className="card-body">
                                 <div className="d-flex justify-content-between mb-3 pt-1">
                                     <h6 className="font-weight-medium">Subtotal</h6>
-                                    <h6 className="font-weight-medium">$150</h6>
+                                    <h6 className="font-weight-medium">&#8377;{total}</h6>
                                 </div>
                                 <div className="d-flex justify-content-between">
                                     <h6 className="font-weight-medium">Shipping</h6>
-                                    <h6 className="font-weight-medium">$10</h6>
+                                    <h6 className="font-weight-medium">&#8377;{shipping}</h6>
                                 </div>
                             </div>
                             <div className="card-footer border-secondary bg-transparent">
                                 <div className="d-flex justify-content-between mt-2">
                                     <h5 className="font-weight-bold">Total</h5>
-                                    <h5 className="font-weight-bold">$160</h5>
+                                    <h5 className="font-weight-bold">&#8377;{final}</h5>
                                 </div>
-                                <button className="btn btn-block btn-primary my-3 py-3">Proceed To Checkout</button>
+                                <Link to="/checkout" className="btn btn-block btn-primary my-3 py-3">Proceed To Checkout</Link>
                             </div>
                         </div>
                     </div>
